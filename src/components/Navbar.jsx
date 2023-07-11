252e54517c4735dba339db214be13870c8fa6ac4
Akhil P L <akhilplx@gmail.com>
2023-07-11 19:06:18 +0530

    Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,28 +12,27 @@ function Navbar() {
 
 
   return (
-    <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-yellow-300'>
+    <div className=' font-burtons flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 dark:text-yellow-50'>
         <h1 className=' w-full text-3xl font-bold'>Go To</h1>
         {/* for bigger devices */}
         <ul className='hidden md:flex'>
-            <NavLink to={'/'} className='p-4'>Home</NavLink>
-            <NavLink to={'/About'} className='p-4'>About</NavLink>
-            <NavLink to={'/Work'} className='p-4'>Works</NavLink>
-            <NavLink to={'/Contact'} className='p-4'>Contacts</NavLink>
+            <NavLink to={'/'} className={({isActive}) => isActive ? "text-teal-600 underline underline-offset-4 text-2xl p-4" : 'p-4' } >Home</NavLink>
+            <NavLink to={'/About'} className={({isActive}) => isActive ? "text-teal-600 underline underline-offset-4 text-2xl p-4" : 'p-4' }>About</NavLink>
+            <NavLink to={'/Work'} className={({isActive}) => isActive ? "text-teal-600 underline underline-offset-4 text-2xl p-4" : 'p-4' }>Works</NavLink>
+            <NavLink to={'/Contact'} className={({isActive}) => isActive ? "text-teal-600 underline underline-offset-4 text-2xl p-4" : 'p-4' }>Contacts</NavLink>
             
         </ul>
         {/* for mobile devices */}
         <div onClick={handleNav} className=' block md:hidden'>
             { nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
         </div>
-        <div className={ nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-800 ease-in-out duration-700 ' : 'fixed left-0' }>
+        <div className={ nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-slate-300 dark:bg-gray-800 ease-in-out duration-700 ' : 'fixed left-0' }>
             {/* <h1 className=' md:hidden w-full text-3xl font-bold m-4'>Select</h1> */}
             <ul className=' md:hidden pt-24 uppercase p-4'>
-                <NavLink to={'/'} onClick={handleNav} className='p-4 border-b border-gray-600 block'>Home</NavLink>
-                <NavLink to={'/About'} onClick={handleNav} className='p-4 border-b border-gray-600 block'>About</NavLink>
-                <NavLink to={'/Work'} onClick={handleNav} className='p-4 border-b border-gray-600 block'>Works</NavLink>
-                <NavLink to={'/Contact'} onClick={handleNav} className='p-4 block'>Contacts</NavLink>
-                
+                <NavLink to={'/'} onClick={handleNav} className={({isActive}) => isActive ? "text-teal-600 text-2xl p-4 border-b border-gray-600 block" : 'p-4 border-b border-gray-600 block' }>Home</NavLink>
+                <NavLink to={'/About'} onClick={handleNav} className={({isActive}) => isActive ? "text-teal-600 text-2xl p-4 border-b border-gray-600 block" : 'p-4 border-b border-gray-600 block' }>About</NavLink>
+                <NavLink to={'/Work'} onClick={handleNav} className={({isActive}) => isActive ? "text-teal-600 text-2xl p-4 border-b border-gray-600 block" : 'p-4 border-b border-gray-600 block' }>Works</NavLink>
+                <NavLink to={'/Contact'} onClick={handleNav} className={({isActive}) => isActive ? "text-teal-600 text-2xl p-4 block" : 'p-4 block' }>Contacts</NavLink>
             </ul>
         </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,14 +18,14 @@ function Sidebar(props) {
   return (
     
     <section className="min-h-screen lg:bg-gray-800">
-        <nav className=' px-5 pt-10 pb-5 mb-12 flex justify-between'>
+        <nav className=' px-5 pt-10 mb-12 flex justify-between'>
             <h1 className=' text-xl font-burtons dark:text-yellow-50 lg:text-yellow-50'>Hello World!</h1>            <ul className=' flex items-center'>
                 <li>
                     <p className=' block dark:hidden lg:text-yellow-50'>Dark Mode</p>
                     <p className=' hidden dark:block text-yellow-50'>Light Mode</p>
                 </li>
-                <li>
-                    <BsFillMoonStarsFill onClick={() => props.setDarkMode(!props.darkMode) } className=' cursor-pointer text-2xl dark:text-yellow-50 ' />
+                <li className=' pl-2'>
+                    <BsFillMoonStarsFill onClick={() => props.setDarkMode(!props.darkMode) } className=' cursor-pointer text-2xl dark:text-yellow-50 lg:text-yellow-50 ' />
                 </li>
                 <li>
                     <button
@@ -40,18 +40,18 @@ function Sidebar(props) {
         <div className="grid grid-cols-1 md:grid-cols-2 md:text-left lg:flex lg:flex-wrap lg:justify-end">
             <div className='flex items-center justify-end'>
                 <div className=' text-right px-10'>
-                    <h2 className=' text-5xl py-2 text-teal-600 font-extrabold md:text-6xl lg:text-7xl'>
+                    <h2 className=' text-5xl text-teal-600 font-extrabold md:text-6xl'>
                         AKHIL<br></br>P L
                     </h2>
                     <div>
-                        <h3 className=' font-burtons text-2xl py-2 md:text-3xl lg:text-3xl dark:text-yellow-50 lg:text-yellow-50'>
+                        <h3 className=' font-burtons text-2xl py-2 md:text-3xl lg:text-2xl dark:text-yellow-50 lg:text-yellow-50'>
                             <Typed strings={['PYTHON DEVELOPER', 'WEB DEVELOPER', 'DATA ANALYST']} typeSpeed={100} backSpeed={40} loop />
                         </h3>
                     </div>
                 </div>
             </div>
             
-            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:w-96 md:h-96 '>
+            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-14 overflow-hidden md:w-96 md:h-96 '>
                 <img src={pic} layout='fill' objectFit='cover' />
             </div>
         </div>
